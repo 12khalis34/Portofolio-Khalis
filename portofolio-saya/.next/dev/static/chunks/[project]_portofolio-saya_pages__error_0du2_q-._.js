2b61b69816c9e6356670811578ba9780340a68b5
Fakecyber21 <almuqarrabink@gmail.com>
2026-06-27 18:34:39 +0700

    Memperbarui urutan halaman, nama sertifikat, dan memperbaiki responsif UI

---
+++ portofolio-saya/.next/dev/static/chunks/[project]_portofolio-saya_pages__error_0du2_q-._.js
@@ -0,0 +1,5 @@
+(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: ["static/chunks/node_modules_next_dist_compiled_14ibvna._.js","static/chunks/node_modules_next_dist_shared_lib_12jmhie._.js","static/chunks/node_modules_next_dist_client_16lnmlo._.js","static/chunks/node_modules_next_dist_0zk-zb1._.js","static/chunks/0-fb_next_dist_pages__error_0hwu17-.js","static/chunks/[next]_entry_page-loader_ts_088muwr._.js","static/chunks/node_modules_react-dom_0kla17-._.js","static/chunks/node_modules_1u6sn6y._.js","static/chunks/[root-of-the-server]__01c5bhz._.js"],
+    source: "entry"
+});
---
+++ portofolio-saya/.next/dev/static/chunks/pages/_error.js
@@ -0,0 +1,13 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/node_modules_next_dist_compiled_14ibvna._.js",
+  "static/chunks/node_modules_next_dist_shared_lib_12jmhie._.js",
+  "static/chunks/node_modules_next_dist_client_16lnmlo._.js",
+  "static/chunks/node_modules_next_dist_0zk-zb1._.js",
+  "static/chunks/0-fb_next_dist_pages__error_0hwu17-.js",
+  "static/chunks/[next]_entry_page-loader_ts_088muwr._.js",
+  "static/chunks/node_modules_react-dom_0kla17-._.js",
+  "static/chunks/node_modules_1u6sn6y._.js",
+  "static/chunks/[root-of-the-server]__01c5bhz._.js",
+  "static/chunks/[project]_portofolio-saya_pages__error_0du2_q-._.js",
+  "static/chunks/turbopack-[project]_portofolio-saya_pages__error_0k9rti-._.js"
+])
